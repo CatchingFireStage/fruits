d2a8c9f7bb47c02a1a94aea44b3008b51cf47ed3
qixi <qixi@beihai365.com>
2021-08-04 18:13:20 +0800

    swagger

---
+++ src/main/java/me/fruits/fruits/configuration/SwaggerConfiguration.java
@@ -0,0 +1,100 @@
+package me.fruits.fruits.configuration;
+
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.schema.WildcardType;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.service.Tag;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket petApi(TypeResolver typeResolver){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .alternateTypeRules(
+                        newRule(typeResolver.resolve(DeferredResult.class,
+                                typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+                                typeResolver.resolve(WildcardType.class)))
+                .useDefaultResponseMessages(false)
+                .globalResponses(HttpMethod.GET,
+                        singletonList(new ResponseBuilder()
+                                .code("500")
+                                .description("500 message")
+                                .representation(MediaType.TEXT_XML)
+                                .apply(r ->
+                                        r.model(m ->
+                                                m.referenceModel(ref ->
+                                                        ref.key(k ->
+                                                                k.qualifiedModelName(q ->
+                                                                        q.namespace("some:namespace")
+                                                                                .name("ERROR"))))))
+                                .build()))
+                .securitySchemes(singletonList(apiKey()))
+                .securityContexts(singletonList(securityContext()))
+                .enableUrlTemplating(true)
+                //全局属性设置
+                .globalRequestParameters(
+                        singletonList(new springfox.documentation.builders.RequestParameterBuilder()
+                                .name("水果项目")
+                                .description("水果项目描述")
+                                .in(ParameterType.QUERY)
+                                .required(true)
+                                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                                .build()))
+                .tags(new Tag("Pet Service", "All apis relating to pets"));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("mykey", "api_key", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/anyPath.*"))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return singletonList(
+                new SecurityReference("mykey", authorizationScopes));
+    }
+
+
+}
