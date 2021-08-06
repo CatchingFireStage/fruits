7213ae8b7c587cdfecfc499dd9acfb2c2558b28c
qixi <qixi@beihai365.com>
2021-08-06 17:35:43 +0800

    后台登录接口

--- src/main/java/me/fruits/fruits/controller/admin/AssistController.java
+++ src/main/java/me/fruits/fruits/controller/admin/AssistController.java
@@ -2,13 +2,11 @@ package me.fruits.fruits.controller.admin;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import me.fruits.fruits.service.admin.LoginService;
 import me.fruits.fruits.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 
 @RequestMapping("/assist")
 @RestController(value = "AdminAssistController")
@@ -18,14 +16,6 @@ public class AssistController {
     @Autowired
     private LoginService loginService;
 
-    @ApiOperation("生成登录token")
-    @GetMapping("make/token")
-    public HashMap<String, String> makeToken(
-            @ApiParam("成员id") @RequestParam Long userId
-    ) {
-
-        return new HashMap<String, String>();
-    }
 
     @ApiOperation("登录")
     @PostMapping("login")
