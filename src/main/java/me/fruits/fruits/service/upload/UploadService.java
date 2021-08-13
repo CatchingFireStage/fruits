55f4022895000beca969c7f533d9a1d582e6920f
qixi <qixi@beihai365.com>
2021-08-13 11:14:24 +0800

    update

--- src/main/java/me/fruits/fruits/service/upload/UploadService.java
+++ src/main/java/me/fruits/fruits/service/upload/UploadService.java
@@ -1,7 +1,6 @@
 package me.fruits.fruits.service.upload;
 
 import lombok.extern.slf4j.Slf4j;
-import me.fruits.fruits.service.upload.vo.ImageVO;
 import me.fruits.fruits.utils.FruitsException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
