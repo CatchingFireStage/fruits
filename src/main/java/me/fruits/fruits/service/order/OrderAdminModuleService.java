0810e1b7e0399a2ba7c9c6575eb1e5b16c2e447f
qixi <qixi@beihai365.com>
2021-08-19 15:03:12 +0800

    update

--- src/main/java/me/fruits/fruits/service/order/OrderAdminModuleService.java
+++ src/main/java/me/fruits/fruits/service/order/OrderAdminModuleService.java
@@ -5,5 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class OrderAdminModuleService extends OrderService{
+public class OrderAdminModuleService extends OrderService {
+
+
 }
--- src/main/java/me/fruits/fruits/task/order/OrderTask.java
+++ src/main/java/me/fruits/fruits/task/order/OrderTask.java
@@ -1,9 +1,17 @@
 package me.fruits.fruits.task.order;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import me.fruits.fruits.mapper.OrdersMapper;
+import me.fruits.fruits.mapper.po.Orders;
+import me.fruits.fruits.service.order.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Random;
 
 /**
  * 定时任务
@@ -13,10 +21,35 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class OrderTask {
 
+    @Autowired
+    private OrdersMapper ordersMapper;
+
+    @Autowired
+    private OrderService orderService;
+
+    private Random random = new Random();
+
     //fixedDelay和fixedRate，单位是毫秒
     //cron是格式
-    @Scheduled(fixedDelay = 2 * 1000)
-    public void testOrderPayAndOrderClose(){
-        log.info("使用FixedDelay  testOrderPayAndOrderClose 线程名称：{}",Thread.currentThread().getName());
+    @Scheduled(fixedDelay = 5 * 1000)
+    public void testOrderPayAndOrderClose() {
+
+        QueryWrapper<Orders> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("state", 0);
+
+
+        List<Orders> orders = this.ordersMapper.selectList(queryWrapper);
+
+        orders.forEach(order -> {
+
+            if ((random.nextInt() % 5) == 0) {
+                //关闭订单
+                orderService.updateStatusToClose(order.getId());
+            } else {
+                //支付成功
+                orderService.updateStatusToPay(order.getId());
+            }
+        });
+
     }
 }
