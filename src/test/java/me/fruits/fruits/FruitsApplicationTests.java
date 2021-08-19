cbe94e900efb36a8f39cf69e953768e154d50e9a
qixi <qixi@beihai365.com>
2021-08-19 15:49:38 +0800

    spring boot 执行单元测试的时候，因为引入的websocket，websocket启动需要依赖tomcat，所以单元测试的时候真正启动一个tomacat

--- src/test/java/me/fruits/fruits/FruitsApplicationTests.java
+++ src/test/java/me/fruits/fruits/FruitsApplicationTests.java
@@ -3,7 +3,13 @@ package me.fruits.fruits;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/**
+ * 在SpringBootTest后加上
+ *
+ * (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) 即可
+ * 原因:websocket是需要依赖tomcat等容器的启动。所以在测试过程中我们要真正的启动一个tomcat作为容器。
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FruitsApplicationTests {
 
     @Test
