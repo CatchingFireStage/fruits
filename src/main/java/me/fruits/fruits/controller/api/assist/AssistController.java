72da3303c0d09e972b072fcf6c501c04091a8a33
qixi <qixi@beihai365.com>
2021-09-02 15:06:11 +0800

    update

--- src/main/java/me/fruits/fruits/controller/api/assist/AssistController.java
+++ src/main/java/me/fruits/fruits/controller/api/assist/AssistController.java
@@ -3,6 +3,8 @@ package me.fruits.fruits.controller.api.assist;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import me.fruits.fruits.service.user.UserApiModuleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +16,11 @@ import java.util.HashMap;
 @RestController(value = "ApiAssistController")
 @Api(tags = "打辅助")
 public class AssistController {
-    
+
+
+    @Autowired
+    private UserApiModuleService userApiModuleService;
+
     @ApiOperation("生成登录token")
     @GetMapping("make/token")
     public HashMap<String, String> makeToken(
---
+++ src/main/java/me/fruits/fruits/service/user/UserApiModuleService.java
@@ -0,0 +1,7 @@
+package me.fruits.fruits.service.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserApiModuleService extends UserService{
+}
