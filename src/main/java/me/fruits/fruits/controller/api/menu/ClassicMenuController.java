40d80317804ef42c752afb0d76d090b288a6c62f
qixi <qixi@beihai365.com>
2021-09-03 15:08:20 +0800

    经典菜单

--- src/main/java/me/fruits/fruits/controller/api/menu/ClassicMenuController.java
+++ src/main/java/me/fruits/fruits/controller/api/menu/ClassicMenuController.java
@@ -8,6 +8,7 @@ import me.fruits.fruits.controller.ApiLogin;
 import me.fruits.fruits.mapper.SpuMapper;
 import me.fruits.fruits.mapper.po.Spu;
 import me.fruits.fruits.service.spu.WrapperDTOService;
+import me.fruits.fruits.service.spu.dto.SpuCategoryDTO;
 import me.fruits.fruits.service.spu.dto.SpuDTO;
 import me.fruits.fruits.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequestMapping("/menu")
 @RestController(value = "apiClassicMenuController")
@@ -34,16 +39,45 @@ public class ClassicMenuController {
 
     @ApiOperation(value = "经典菜单")
     @GetMapping("/menu")
-    public Result<List<SpuDTO>> menu(){
+    public Result<List<Object>> menu() {
 
 
         QueryWrapper<Spu> queryWrapper = new QueryWrapper<>();
         //有货
-        queryWrapper.eq("is_inventory",1);
+        queryWrapper.eq("is_inventory", 1);
         List<Spu> spus = spuMapper.selectList(queryWrapper);
+
 
         List<SpuDTO> spuDTOS = wrapperDTOService.wrapperSPUs(spus);
 
-        return Result.success(spuDTOS);
+        //获取所有类别
+        Map<Long, SpuCategoryDTO> categoryMapKeyIsCategoryId = spuDTOS.stream().map(SpuDTO::getCategory).collect(Collectors.toMap(
+                SpuCategoryDTO::getId, spuCategoryDTO -> spuCategoryDTO,
+                //如果key重复，取后面的值覆盖
+                (last,next) -> next
+                ));
+
+        //获取spu数据，然后按照类别分组
+        Map<Long, List<SpuDTO>> goodsMapKeyIsCategoryId = spuDTOS.stream().collect(Collectors.groupingBy(spu -> {
+            return spu.getCategory().getId();
+        }));
+
+
+        List<Object> response = new ArrayList<>();
+        categoryMapKeyIsCategoryId.forEach((key,value) -> {
+
+            Map<String,Object> item = new HashMap<>();
+            //类别数据
+            item.put("id",value.getId());
+            item.put("name",value.getName());
+
+            //类别下的所有商品
+            item.put("goods",goodsMapKeyIsCategoryId.get(value.getId()));
+
+
+            response.add(item);
+        });
+
+        return Result.success(response);
     }
 }
