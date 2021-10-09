4b49323a5142f0192118349e9b83cab3535c979b
qixi <qixi@beihai365.com>
2021-10-09 16:45:29 +0800

    订单状态枚举bug

--- src/main/java/me/fruits/fruits/mapper/enums/OrderStateEnum.java
+++ src/main/java/me/fruits/fruits/mapper/enums/OrderStateEnum.java
@@ -2,11 +2,11 @@ package me.fruits.fruits.mapper.enums;
 
 public enum OrderStateEnum implements EnumLabelValue {
 
-    ORDER(0,"下单"),
-    PAY(1,"已支付"),
-    CLOSE(2,"订单关闭（未支付成功，支付失败）"),
-    COMPLETED(3,"制作完成"),
-    DELIVERY(4,"已取餐");
+    ORDER(0, "下单"),
+    PAY(1, "已支付"),
+    CLOSE(2, "订单关闭（未支付成功，支付失败）"),
+    COMPLETED(3, "制作完成"),
+    DELIVERY(4, "已取餐");
 
 
     /**
@@ -27,11 +27,11 @@ public enum OrderStateEnum implements EnumLabelValue {
 
     @Override
     public String getLabel() {
-        return null;
+        return this.label;
     }
 
     @Override
     public Integer getValue() {
-        return null;
+        return this.value;
     }
 }
