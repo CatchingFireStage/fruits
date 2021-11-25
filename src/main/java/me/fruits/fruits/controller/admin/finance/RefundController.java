ae01f08dec1c6e229b06c52a504e660b74094729
qixi <qixi@beihai365.com>
2021-11-25 10:12:16 +0800

    退款列表

--- src/main/java/me/fruits/fruits/controller/admin/finance/RefundController.java
+++ src/main/java/me/fruits/fruits/controller/admin/finance/RefundController.java
@@ -1,20 +1,26 @@
 package me.fruits.fruits.controller.admin.finance;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import me.fruits.fruits.controller.AdminLogin;
 import me.fruits.fruits.controller.admin.finance.vo.AddRefundRequest;
+import me.fruits.fruits.mapper.enums.pay.refund.RefundStateEnum;
+import me.fruits.fruits.mapper.po.Refund;
+import me.fruits.fruits.service.pay.refund.RefundAdminModuleService;
 import me.fruits.fruits.service.pay.refund.RefundService;
-import me.fruits.fruits.utils.MoneyUtils;
-import me.fruits.fruits.utils.PageVo;
-import me.fruits.fruits.utils.Result;
+import me.fruits.fruits.utils.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/finance")
 @RestController(value = "AdminRefundController")
@@ -27,17 +33,56 @@ public class RefundController {
     @Autowired
     private RefundService refundService;
 
+    @Autowired
+    private RefundAdminModuleService refundAdminModuleService;
+
     @GetMapping(value = "/refund")
     @ApiOperation("退款-列表")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "keyword", value = "payid 支付表的id", example = "")
+            @ApiImplicitParam(name = "keyword", value = "payid 支付表的id", example = ""),
+            @ApiImplicitParam(name = "state", value = "退款状态过滤,REFUND退款中,SUCCESS退款成功,ABNORMAL退款异常,CLOSE退款关闭", example = "")
     })
     public Result<Object> refund(
-            @RequestParam(required = false) String keyword,
+            @RequestParam(required = false, defaultValue = "") String keyword,
+            @RequestParam(required = false, defaultValue = "") String state,
             PageVo pageVo
     ) {
 
-        return null;
+        RefundStateEnum refundStateEnum;
+        try {
+
+            refundStateEnum = RefundStateEnum.valueOf(state);
+        } catch (IllegalArgumentException e) {
+            refundStateEnum = null;
+        }
+
+        //获取数据
+        IPage<Refund> refunds = refundAdminModuleService.getRefunds(keyword, refundStateEnum, pageVo);
+
+        if (refunds.getRecords().size() <= 0) {
+            return Result.success(refunds.getTotal(), refunds.getPages(), new ArrayList<>());
+        }
+
+        //构建返回数据
+        List<Object> list = new ArrayList<>();
+
+        refunds.getRecords().forEach(refund -> {
+
+            Map<String, Object> refundItem = new HashMap<>();
+
+            refundItem.put("id", refund.getId());
+            refundItem.put("amount", MoneyUtils.fenChangeYuan(refund.getAmount()));
+            refundItem.put("outRefundNo", refund.getOutRefundNo());
+            refundItem.put("createTime", DateFormatUtils.format(refund.getCreateTime()));
+            refundItem.put("payId", refund.getPayId());
+            refundItem.put("reason", refund.getReason());
+            refundItem.put("state", EnumUtils.changeToString(RefundStateEnum.class, refund.getState()));
+
+
+            list.add(refundItem);
+        });
+
+        return Result.success(refunds.getTotal(), refunds.getPages(), list);
     }
 
 
@@ -56,7 +101,7 @@ public class RefundController {
     public Result<String> reiterateRefund(@PathVariable long id) {
 
         refundService.reiterateRefund(id);
-        
+
         return Result.success();
     }
 }
---
+++ src/main/java/me/fruits/fruits/service/pay/refund/RefundAdminModuleService.java
@@ -0,0 +1,41 @@
+package me.fruits.fruits.service.pay.refund;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import me.fruits.fruits.mapper.RefundMapper;
+import me.fruits.fruits.mapper.enums.pay.refund.RefundStateEnum;
+import me.fruits.fruits.mapper.po.Refund;
+import me.fruits.fruits.utils.PageVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RefundAdminModuleService {
+
+
+    @Autowired
+    private RefundMapper refundMapper;
+
+    /**
+     * 获取退款列表
+     *
+     * @param keyword     搜索关键字
+     * @param refundState 退款状态
+     */
+    public IPage<Refund> getRefunds(String keyword, RefundStateEnum refundState, PageVo pageVo) {
+
+        QueryWrapper<Refund> queryWrapper = new QueryWrapper<>();
+
+        if (keyword != null && !keyword.isEmpty()) {
+            //支付id搜索
+            queryWrapper.eq("pay_id", keyword);
+        }
+
+        if (refundState != null) {
+            queryWrapper.eq("state", refundState.getValue());
+        }
+
+        return refundMapper.selectPage(new Page<>(pageVo.getP(), pageVo.getPageSize()), queryWrapper);
+    }
+}
