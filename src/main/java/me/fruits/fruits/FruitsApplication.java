3761daa3c9bda233c4b4acf50eb7449e0d954dcf
qixi <qixi@beihai365.com>
2021-11-25 17:00:23 +0800

    时区在代码中设置

--- src/main/java/me/fruits/fruits/FruitsApplication.java
+++ src/main/java/me/fruits/fruits/FruitsApplication.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @MapperScan("me.fruits.fruits.mapper")
@@ -20,13 +22,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class FruitsApplication {
 
     public static void main(String[] args) {
+
+        //设置时区
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+
+
         ConfigurableApplicationContext run = SpringApplication.run(FruitsApplication.class, args);
 
         //打印所有已经载入的bean
         String[] beanDefinitionNames = run.getBeanDefinitionNames();
-        log.info("bean总数:{}",run.getBeanDefinitionCount());
-        for (int i=0; i< run.getBeanDefinitionCount(); i++){
-            log.info("第{}个,beanName:{}",i,beanDefinitionNames[i]);
+        log.info("bean总数:{}", run.getBeanDefinitionCount());
+        for (int i = 0; i < run.getBeanDefinitionCount(); i++) {
+            log.info("第{}个,beanName:{}", i, beanDefinitionNames[i]);
         }
     }
 
